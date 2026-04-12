0e35bf3d7c86468fe675a9a316181598cd63134f
Lokesh Erugu <119585710+Lokesherugu@users.noreply.github.com>
2026-04-12 23:07:44 +0530

    Hide database password in DATABASE_URL
    Obscured the database password in the DATABASE_URL.

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:123qwe@localhost/task_manager"
+DATABASE_URL = "mysql+pymysql://root:*****@localhost/task_manager"
 
 engine = create_engine(DATABASE_URL)
 
